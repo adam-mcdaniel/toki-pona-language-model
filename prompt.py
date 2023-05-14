1b9cca43c1e32efc7a275ff5ebc754dcbd3ce4d5
Adam McDaniel <adam.mcdaniel17@gmail.com>
2023-05-14 00:17:41 -0400

    Added code files from dataset

--- prompt.py
+++ prompt.py
@@ -7,12 +7,12 @@ try:
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_fast=True)
 
     model_options = [
-        ("adamthekiwi/toki-pona-gpt2-alpaca-best", "Pretrained GPT2 on GPT4 alpaca dataset. Trained with encyclopedia data. Uses same data as non-alpaca models but with alpaca prompt formatting"),
-        ("adamthekiwi/toki-pona-gpt2-alpaca-better", "Pretrained GPT2 on GPT4 alpaca dataset. Trained with more variation in prompts. Uses same data as non-alpaca models but with alpaca prompt formatting"),
-        ("adamthekiwi/toki-pona-gpt2-alpaca", "Pretrained GPT2 on GPT4 alpaca dataset. Uses same data as non-alpaca models but with alpaca prompt formatting"),
-        ("adamthekiwi/toki-pona-gpt2", "GPT2 trained on toki-pona data with custom prompt formatting"),
-        ("adamthekiwi/toki-pona-better", "DistilGPT2 trained on toki-pona data with more prompt data, more epochs, and warmup steps"),
         ("adamthekiwi/toki-pona", "DistilGPT2 trained on toki-pona data with custom prompt formatting"),
+        ("adamthekiwi/toki-pona-better", "DistilGPT2 trained on toki-pona data with more prompt data, more epochs, and warmup steps"),
+        ("adamthekiwi/toki-pona-gpt2-alpaca-better", "Pretrained GPT2 on GPT4 alpaca dataset. Trained with more variation in prompts. Uses same data as non-alpaca models but with alpaca prompt formatting"),
+        ("adamthekiwi/toki-pona-gpt2-alpaca-best", "Pretrained GPT2 on GPT4 alpaca dataset. Trained with encyclopedia data. Uses same data as non-alpaca models but with alpaca prompt formatting"),
+        # ("adamthekiwi/toki-pona-gpt2-alpaca", "Pretrained GPT2 on GPT4 alpaca dataset. Uses same data as non-alpaca models but with alpaca prompt formatting"),
+        # ("adamthekiwi/toki-pona-gpt2", "GPT2 trained on toki-pona data with custom prompt formatting"),
     ]
 
     chosen_model = model_options[0]
@@ -51,10 +51,27 @@ try:
         print('Encoding prompt...')
         prompt = input('Enter an instruction prompt: ')
 
-        input_ids = tokenizer.encode(f'''Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{prompt}\n\n### Response:\n''', return_tensors='pt')
+        # input_ids = tokenizer.encode(f'''{prompt}\n=====\n''', return_tensors='pt')
+        # print('Generating...')
+        prompt = f'''Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{prompt}\n\n### Response:\n'''
+        input_ids = tokenizer.encode(prompt, return_tensors='pt')
+        print(prompt)
+        
+        # output = model_test.generate(input_ids, max_length=max_length, num_beams=num_beams, no_repeat_ngram_size=3, early_stopping=True)
+        # print(f'{tokenizer.decode(output[0], skip_special_tokens=True)}')
+        # input_ids = tokenizer.encode(f'''{prompt}\n=====\n''', return_tensors='pt')
+        # input_ids = tokenizer.encode(f'''{prompt}\n>>>>>\n''', return_tensors='pt')
         print('Generating...')
-        output = model_test.generate(input_ids, max_length=max_length, num_beams=num_beams, no_repeat_ngram_size=3, early_stopping=True)
+        try:
+            output = model_test.generate(input_ids, max_length=max_length, num_beams=num_beams, no_repeat_ngram_size=3, early_stopping=True)
+        except KeyboardInterrupt:
+            continue
+        except Exception as e:
+            print(e)
+            exit(1)
+
         print(f'{tokenizer.decode(output[0], skip_special_tokens=True)}')
+        # input_ids = tokenizer.encode(f'''Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{prompt}\n\n### Response:\n''', return_tensors='pt')
 except KeyboardInterrupt:
     print('Goodbye!')
     exit(0)
--- toki-pona-dataset/README.md
+++
@@ -1,4 +0,0 @@
-# toki-pona-dataset
-
-This is a repository containing a dataset for the Toki Pona language. It contains documents in Toki Pona, and some translations into English. It also contains a list of phrases translated into English, Toki Pona, and Chinese.
-
